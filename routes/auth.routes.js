8d6e5706e48ba60b8fb60d8d3852acd424531c85
paulocaetano96 <paulocaetano96@gmail.com>
2023-03-13 18:19:04 +0000

    auth.routes update

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -103,10 +103,10 @@ router.post("/login", (req, res, next) => {
 
       if (passwordCorrect) {
         // Deconstruct the user object to omit the password
-        const { _id, email, name, role } = foundUser;
+        const { _id, email, name, role, club } = foundUser;
 
         // Create an object that will be set as the token payload
-        const payload = { _id, email, name, role };
+        const payload = { _id, email, name, role, club };
 
         // Create a JSON Web Token and sign it
         const authToken = jwt.sign(payload, process.env.TOKEN_SECRET, {
